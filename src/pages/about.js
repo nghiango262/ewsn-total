5a896302f6ef693e45e5e6c4a663ca4e649df07d
NgoNGHIA <nghiango262@gmail.com>
2019-12-11 17:49:40 +0700

    xong header view với nav

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../layout/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+    <Layout>
+    <SEO title="Home" />
+        <h1>Cai quan ne ban</h1>
+        <h1>Cai quan ne ban</h1>
+        <h1>Cai quan ne ban</h1>
+        <h1>Cai quan ne ban</h1>
+        <h1>Cai quan ne ban</h1>
+    </Layout>
+)
+
+export default AboutPage
