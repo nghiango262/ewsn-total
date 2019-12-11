d721516a54121f58483c2bfc0c79026d44f63a35
NgoNGHIA <nghiango262@gmail.com>
2019-12-11 14:09:03 +0700

    change title and using scss for style

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,32 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Lorem</p>
+
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>Lorem</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
